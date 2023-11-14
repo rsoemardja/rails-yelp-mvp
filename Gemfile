002428d20c1660f424f333e1ea4758c4b49a1519
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2023-11-14 12:43:01 +1100

    Finished the Yelp Appp

--- Gemfile
+++ Gemfile
@@ -61,3 +61,8 @@ end
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem 'simple_form'
+
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.15.1'
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts 'Cleaning the database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+5.times do |number|
+
+  Restaurant.create!(
+    name: Faker::TvShows::BojackHorseman.character,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: [ "chinese", "italian", "japanese", "french", "belgian" ].sample
+  )
+end
+
+puts 'Finished creating the restaurants!'
